399190c092d80fb308a0e0247bcfbe5b991e8819
student <student@PC-635.urel.feec.vutbr.cz>
2021-10-13 14:44:00 +0200

    lab04

--- Labs/03-gpio/gpio/main.c
+++ Labs/03-gpio/gpio/main.c
@@ -12,7 +12,7 @@
 
 /* Defines -----------------------------------------------------------*/
 #define LED_GREEN   PB5     // AVR pin where green LED is connected
-#define LED_BLUE    PB4     // AVR pin where blue LED is connected
+#define LED_BLUE    PC4     // AVR pin where blue LED is connected
 #define BUTTON      PD3     // AVR pin where BUTTON is connected
 #define BLINK_DELAY 500
 #ifndef F_CPU
@@ -42,27 +42,21 @@ int main(void)
     GPIO_write_low(&PORTB, LED_BLUE);
 
     // Configure Push button at port D and enable internal pull-up resistor
-    GPIO_config_input_pull(&DDRB, BUTTON);
+    GPIO_config_input_pullup(&DDRD, BUTTON);
     
 
     // Infinite loop
     while (1)
     {
-        // Pause several milliseconds
-        _delay_ms(BLINK_DELAY);
-
-    while(GPIO_read(PIND, BUTTON) == 0)
-    {
-        _delay_ms(BLINK_DELAY);
-
-        PORTB = PORTB | (1<<LED_GREEN);
-        PORTB = PORTB | (1<<LED_BLUE);
-
-        _delay_ms(BLINK_DELAY);
-
-        PORTB = PORTB & ~(1<<LED_GREEN);
-        PORTB = PORTB & ~(1<<LED_BLUE);
-    }
+        if (bit_is_clear(PIND, BUTTON))
+        {
+            // Pause several milliseconds
+            _delay_ms(BLINK_DELAY);
+            
+            GPIO_toggle(&PORTB, LED_GREEN);
+            
+            GPIO_toggle(&PORTC, LED_BLUE);
+        }
         
     }
 
